d1a3a114141f36fabbb51a44bac77a7e014efecd
DungVPT <vuphamtiendung@gmail.com>
2023-12-22 00:13:21 +0700

    Git Commit Change the algorithms

--- Array.BubbleSort/Program.cs
+++ Array.BubbleSort/Program.cs
@@ -3,9 +3,10 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        WriteLine($"Input amount in the array");
+        WriteLine("Input amount in the array");
         int amount = Convert.ToInt32(ReadLine());
         int[] array = new int[amount];
+        int countArray = 0;
 
         WriteLine("Input data in the array");
         for(int index = 0; index < array.Length; index++)
@@ -13,14 +14,15 @@ internal class Program
             array[index] = Convert.ToInt32(ReadLine());
         }
 
+        WriteLine("Data before have bubble sort");
         foreach(var item in array)
         {
-            WriteLine($"Array before in the swap: {item}");
+            WriteLine($"{item}");
         }
 
         for(int index = 0; index < array.Length; index++)
         {
-            for(int indexTwo = index + 1; indexTwo < array.Length; indexTwo++)
+            for (int indexTwo = index + 1; indexTwo < array.Length; indexTwo++)
             {
                 if (array[index] > array[indexTwo])
                 {
@@ -30,12 +32,12 @@ internal class Program
                 }
             }
         }
-
-        foreach(var item in array)
+        WriteLine("Array after have the bubble sort:");
+        foreach (var item in array.Distinct())
         {
-            WriteLine($"Array after in the swap: {item}");
+            
+            WriteLine($"{item}");
         }
-
         ReadLine();
     }
 }
